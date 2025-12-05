e1c31d1dd33754de2067db049d9c257b3454cad0
leecyrille <leemaskell@gmail.com>
2025-12-05 08:34:25 -0700

    initial

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'docflow-static-v1';
+const STATIC_ASSETS = ['/', '/index.html', '/manifest.json', '/icons/favicon.svg'];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC_ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+    if (e.request.url.includes('graph.microsoft.com') || e.request.url.includes('/api/')) return;
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        if (res.ok) { const clone = res.clone(); caches.open(CACHE_NAME).then(c => c.put(e.request, clone)); }
+        return res;
+    }).catch(() => caches.match('/index.html'))));
+});
